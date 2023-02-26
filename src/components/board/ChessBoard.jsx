106e65d1e8f75fc875048411dfa3de5fa3ed28d5
Vaishnavi Paliya <vaishnavipaliya11@gmail.com>
2023-02-26 17:35:18 +0530

    fix: moves bug

--- src/components/board/ChessBoard.jsx
+++ src/components/board/ChessBoard.jsx
@@ -10,72 +10,86 @@ export const ChessBoard = () => {
   };
   return (
     <div className="wrapper">
-      <div className="board">
-        {boardMatrix?.map(([i,j]) => {
-          const isBlack = (i + j) % 2 === 0 ? false : true;
-          return (
-            
-            <div
-              className={`cell ${isBlack ? "black" : "white"} ${
-                availableMoves?.find(([m, n]) => m === i && n === j)
-                  ? "highlight"
-                  : ""
-              }`}
-            >
-              {i === 7 || i === 2 ? (
-                <span
-                  onClick={() => {
-                    i === 7
-                      ? handleMove([
-                          [i - 1, j],
-                          [i - 2, j],
-                        ])
-                      : handleMove([
-                          [i + 1, j],
-                          [i + 2, j],
-                        ]);
-                  }}
-                  style={{ color: "red", fontSize: "10px" }}
-                >
-                  Pawn
-                </span>
-              ) : null}
+    <div className="board">
+      {boardMatrix?.map(([i, j]) => {
+        const isBlack = (i + j) % 2 === 0 ? false : true;
+        return (
+          <div key={`${i} ${j}`}
+            className={`cell ${isBlack ? "black" : "white"} ${
+              availableMoves?.find(([m, n]) => m === i && n === j)
+                ? "highlight"
+                : ""
+            }`}
+          >
+            {i === 7 || i === 2 ? (
+              <span
+                onClick={() => {
+                  i === 7
+                    ? handleMove([
+                        [i - 1, j],
+                        [i - 2, j]
+                      ])
+                    : handleMove([
+                        [i + 1, j],
+                        [i + 2, j]
+                      ]);
+                }}
+                className="piece"
+              >
+                <img
+                  style={{ width: "100%" }}
+                  src={
+                    i === 2
+                      ? "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Chess_pdt45.svg/68px-Chess_pdt45.svg.png"
+                      : "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Chess_plt45.svg/68px-Chess_plt45.svg.png"
+                  }
+                  alt=""
+                  className="piece_img"
+                />
+              </span>
+            ) : null}
 
-              {i === 8 ? (
-                <span
-                  onClick={() => {
-                    j > 5
-                      ? handleMove(pieceMapping[j - 5]?.b?.possibleMoves?.["2"])
-                      : handleMove(pieceMapping[j]?.b?.possibleMoves?.["1"]);
-                  }}
-                  style={{ color: "red", fontSize: "10px" }}
-                >
-                  {j > 5
-                    ? pieceMapping[j - 5]?.b?.image
-                    : pieceMapping[j]?.b?.image}
-                </span>
-              ) : null}
+            {i === 8 ? (
+              <span
+              onClick={() => {
+                pieceMapping[j]?.b?.possibleMoves
+                  ? handleMove(pieceMapping[j]?.b?.possibleMoves)
+                  : handleMove([]);
+              }}
+                className="piece"
+              >
+                <img
+                  style={{ width: "100%" }}
+                  src={pieceMapping[j]?.w?.image}
+                  alt=""
+                  className="piece_img"
+                />
+              </span>
+            ) : null}
 
-              {i === 1 ? (
-                <span
-                  onClick={() => {
-                    j > 5
-                      ? handleMove(pieceMapping[j - 5]?.b?.possibleMoves?.["2"])
-                      : handleMove(pieceMapping[j]?.b?.possibleMoves?.["1"]);
-                  }}
-                  style={{ color: "red", fontSize: "10px" }}
-                >
-                  {j > 5
-                    ? pieceMapping[j - 5]?.w?.image
-                    : pieceMapping[j]?.w?.image}
-                </span>
-              ) : null}
+            {i === 1 ? (
+              <span
+              onClick={() => {
+                pieceMapping[j]?.w?.possibleMoves
+                  ? handleMove(pieceMapping[j]?.w?.possibleMoves)
+                  : handleMove([]);
+              }}
+                className="piece"
+              >
+                <img
+                  src={pieceMapping[j]?.b?.image}
+                  alt=""
+                  style={{ width: "100%" }}
+                  className="piece_img"
+                />
+              </span>
+            ) : null}
 
-              {/* <span style={{ color: "red" }}>{`${i},${j}`}</span> */}
-            </div>
-          );
-        })}
-      </div>
+          
+          </div>
+        );
+      })}
     </div>
+  </div>
   );
 };
--- src/data/PeiceMapping.js
+++ src/data/PeiceMapping.js
@@ -1,36 +1,110 @@
 const pieceMapping = {
-    1: {
-      b: { image: "blackRooks", possibleMoves: [] },
-      w: { image: "shiteRooks", possibleMoves: [] }
+  1: {
+    b: {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Chess_rdt45.svg/68px-Chess_rdt45.svg.png",
+      possibleMoves: [],
     },
-    2: {
-      b: {
-        image: "blackKnights",
-        possibleMoves: {
-          1: [
-            [6, 1],
-            [6, 3]
-          ],
-          2: [
-            [6, 6],
-            [6, 8]
-          ]
-        }
-      },
-      w: { image: "whiteknights", possibleMoves: [] }
+    w: {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Chess_rlt45.svg/68px-Chess_rlt45.svg.png",
+      possibleMoves: [],
     },
-    3: {
-      b: { image: "blackBishops", possibleMoves: [] },
-      w: { image: "whiteBishops", possibleMoves: [] }
+  },
+  2: {
+    b: {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Chess_ndt45.svg/68px-Chess_ndt45.svg.png",
+      possibleMoves: [
+        [6, 1],
+        [6, 3],
+      ],
     },
-    4: {
-      b: { image: "whiteQueen", possibleMoves: [] },
-      w: { image: "blackQueen", possibleMoves: [] }
+    w: {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Chess_nlt45.svg/68px-Chess_nlt45.svg.png",
+      possibleMoves: [
+        [3, 1],
+        [3, 3],
+      ],
     },
-    5: {
-      b: { image: "whiteKing", possibleMoves: [] },
-      w: { image: "blackKing", possibleMoves: [] }
-    }
-  };
+  },
+  3: {
+    b: {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Chess_bdt45.svg/68px-Chess_bdt45.svg.png",
+      possibleMoves: [],
+    },
+    w: {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Chess_blt45.svg/68px-Chess_blt45.svg.png",
+      possibleMoves: [],
+    },
+  },
+  4: {
+    b: {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Chess_qdt45.svg/68px-Chess_qdt45.svg.png",
+      possibleMoves: [],
+    },
+    w: {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Chess_qlt45.svg/68px-Chess_qlt45.svg.png",
+      possibleMoves: [],
+    },
+  },
+  5: {
+    b: {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Chess_kdt45.svg/68px-Chess_kdt45.svg.png",
+      possibleMoves: [],
+    },
+    w: {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Chess_klt45.svg/68px-Chess_klt45.svg.png",
+      possibleMoves: [],
+    },
+  },
+  8: {
+    b: {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Chess_rdt45.svg/68px-Chess_rdt45.svg.png",
+      possibleMoves: [],
+    },
+    w: {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Chess_rlt45.svg/68px-Chess_rlt45.svg.png",
+      possibleMoves: [],
+    },
+  },
+  7: {
+    b: {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Chess_ndt45.svg/68px-Chess_ndt45.svg.png",
+      possibleMoves: [
+        [6, 6],
+        [6, 8],
+      ],
+    },
+    w: {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Chess_nlt45.svg/68px-Chess_nlt45.svg.png",
+      possibleMoves: [ [3, 6],
+      [3, 8]],
+    },
+  },
+  6: {
+    b: {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Chess_bdt45.svg/68px-Chess_bdt45.svg.png",
+      possibleMoves: [],
+    },
+    w: {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Chess_blt45.svg/68px-Chess_blt45.svg.png",
+      possibleMoves: [],
+    },
+  },
+};
 
-  export {pieceMapping}
+export { pieceMapping };
